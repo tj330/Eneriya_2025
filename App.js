5cb814a9681e11e8d790361c91abf5d1d82e81e7
SangeethSDevan <23dev500@gmail.com>
2025-02-14 12:00:04 +0530

    ui update

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View ,Text} from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import Header from "./Components/Header";
+import Body from "./Components/Body";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <SafeAreaProvider>
+    <SafeAreaView style={{ flex: 1 ,padding:20}}>  
+      <View  style={s.header}>
+        <Header/>
+      </View>
+      <View style={s.body}>
+        <Body/>
+      </View>
+      <View style={s.footer}>
+        <Text>Footer</Text>
+      </View>
+    </SafeAreaView>
+  </SafeAreaProvider>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const s=StyleSheet.create({
+  header:{
+    flex:1,
   },
-});
+  body:{
+    flex:10,
+  },
+  footer:{
+    flex:1,
+  }
+})
